ba639c4b567f3bfeb0d5e316a7d3081bcb8fd6bc
Rohith <rohithraju@14.7.23.172.generic.clemson.edu>
2016-10-19 17:55:38 -0400

    Initial Commit

---
+++ rraju_assignment4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rraju_assignment4
+//
+//  Created by Rohith Raju on 10/19/16.
+//  Copyright © 2016 Rohith Raju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
